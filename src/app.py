9b2369a113b0a6f2e8807378bb9681713b16b0aa
Petr Wolf <1173038+PetrWolf@users.noreply.github.com>
2024-05-30 22:27:52 +0200

    use a sample app with a test

--- src/app.py
+++ src/app.py
@@ -1,5 +1,12 @@
+"""app.py"""
 import streamlit as st
-import pandas as pd
-import numpy as np
 
-st.title('Hello World!')
+# Initialize st.session_state.beans
+st.session_state.beans = st.session_state.get("beans", 0)
+
+st.title("Bean counter :paw_prints:")
+
+addend = st.number_input("Beans to add", 0, 10)
+if st.button("Add"):
+    st.session_state.beans += addend
+st.markdown(f"Beans counted: {st.session_state.beans}")
