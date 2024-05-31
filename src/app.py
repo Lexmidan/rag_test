bf0ebd351e98052e08c402f35599c6146c93cdae
Petr Wolf <1173038+PetrWolf@users.noreply.github.com>
2024-05-31 21:28:51 +0200

    Attempt at prompt engineering

--- src/app.py
+++ src/app.py
@@ -13,9 +13,11 @@ with st.sidebar:
 @st.cache_data
 def run_query(question: str) -> str:
     hardcoded_context = """
-    Assume a database about TV shows and movies on Netflix with the following tables and columns exists:
+    You are given a database about TV shows and movies on Netflix with the following table and columns in it.
 
-    titles
+    table name: titles
+
+    columns:
     * show_id (string): Unique ID for every Movie / TV Show (example: "s8804")
     * type (string): Identifier - A Movie or TV Show (examples: "Movie", "TV Show")
     * title (string): Title of the Movie / TV Show (example: "Jailbirds New Orleans")
@@ -26,20 +28,51 @@ def run_query(question: str) -> str:
     * release_year (int): Actual Release year of the move / show (example: 2022)
     * rating (string): TV Rating of the movie / show (example: "TV-MA")
     * duration (string): Total Duration - in minutes or number of seasons (examples: "2 Seasons")
-    * listed_in (string): Categories in which the show is listed (example: "Docuseries, Reality TV")
+    * listed_in (string): Comma separated list of categories in which the show is listed (example: "Docuseries, Reality TV")
     * description (string): Description of the Movie / TV show (example: "Dragged from civilian life, a former superhero...")
 
-    Generate a SQL command between <sql> and </sql> that answers user's question.
     """
 
-    prompt = f"""{anthropic.HUMAN_PROMPT} In case you don't have the information in the context provided, please respond with 'I don't know'.
+    prompt = f"""{anthropic.HUMAN_PROMPT} Your goal is to generate a SQL query that answers a user question provided below in the <question> tag, using additional information from <context>. 
+    See <examples> for reference.
+
+    You can only respond in one of two ways:
+    1. If you have enought information to generate a valid SQL query, respond with the SQL code between a pair of <sql> and </sql> tags. Do not output any other text, description or explanation
+    2. Otherwise respond only with the phrase 'I don't know'. Do not use the <sql> or </sql> tags or any other text, description or explanation
+
     <context>
     {hardcoded_context}
     </context>
     <question>
     {question}
     </question>
-    {anthropic.AI_PROMPT}"""
+    <examples>
+        <example>
+            <question>How many French TV shows are there?</question>
+            <answer><sql>SELECT COUNT(*) FROM titles WHERE type = 'TV Show' AND country = 'France';</sql></answer>
+        </example>
+        <example>
+            <question>What is the current weather in Paris?</question>
+            <answer>I don't know</answer>
+        </example>
+        <example>
+            <question>In what movie did Leonardo DiCaprio appear together with Kate Winslet? Display name, director and year or release</question>
+            <answer><sql>SELECT title, director, release_year FROM titles WHERE starring LIKE '%Leonardo DiCaprio%' AND starring LIKE '%Kate Winslet%'</sql></answer>
+        </example>
+        <example>
+            <question>Who won the 2024 Ice Hockey Championship?</question>
+            <answer>I don't know</answer>
+        </example>
+        <example>
+            <question>List the titles, actors and descriptions of the 5 most recent TV shows from the US</question>
+            <answer><sql> SELECT title, starring, description FROM titles WHERE type = 'TV Show' AND country = 'United States' ORDER BY date_added DESC LIMIT 5</sql></answer>
+        </example>
+        <example>
+            <question>Who will be the next US president?</question>
+            <answer>I don't know</answer>
+        </example>
+    </examples>
+    {anthropic.AI_PROMPT}<answer>"""
 
     st.write(f"Calling Claude at {datetime.now()}")
 
